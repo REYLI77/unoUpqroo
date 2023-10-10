593986e9ba6f500cd23ac424b950546e82c83d7c
reyli777 <reylichihernandez84@gmail.com>
2023-10-09 20:06:45 -0600

    inicial commit
    tiene 108 cartas

---
+++ UNO.js
@@ -0,0 +1,48 @@
+const rl = required ("readline-syn")
+rl.createInterface ({
+    imput: process.stdin,
+    output: process.stdout
+
+})
+//juego UNO
+var deck = [];
+var colors = ["Yellow","Blue", "Green", "Red"];
+var types = ["Comun", "Especial","Comodin"];
+var card = {
+    color: null, // amarillo, azul , verde , rojo 
+    number: null, // 0-9
+    Type: null // comun, especiales y comodines
+};
+
+for (var cColor = 0; cColor < colors.length; cColor++ ){
+    
+    for (var cNumber = 0; cNumber < 10; cNumber++){
+        
+        deck.push({color: colors[cColor], number: cNumber, type: "comun"});
+    }
+    for (var cNumber1 = 1; cNumber < 10; cNumber1++){
+        
+    }
+    deck.push({color: colors[cColor], number: "+2", type: "comun"});
+    deck.push({color: colors[cColor], number: "+2", type: "comun"});
+    deck.push({color: colors[cColor], number: "B", type: "comun"});
+    deck.push({color: colors[cColor], number: "B", type: "comun"});
+    deck.push({color: colors[cColor], number: "R", type: "comun"});
+    deck.push({color: colors[cColor], number: "R", type: "comun"});
+
+} 
+
+deck.push({color: null, number: "+4", type: "comun"});
+deck.push({color: null, number: "+4", type: "comun"});
+deck.push({color: null, number: "+4", type: "comun"});
+deck.push({color: null, number: "+4", type: "comun"});
+deck.push({color: null, number: "CC", type: "comun"});
+deck.push({color: null, number: "CC", type: "comun"});
+deck.push({color: null, number: "CC", type: "comun"});
+deck.push({color: null, number: "CC", type: "comun"});
+
+console.log(deck);
+
+const players =  rl.question.log("cuantos jugadores seran", {})
+
+console.log(players)
